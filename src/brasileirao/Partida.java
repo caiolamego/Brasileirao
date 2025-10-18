3c13a97b7205cf79815c28c95385c352a44be659
caiolamego <caio.lamego04@gmail.com>
2025-10-18 12:17:49 -0300

    Ciclo 1 (GREEN) - Teste passando, mas sem triangulação

--- src/brasileirao/Partida.java
+++ src/brasileirao/Partida.java
@@ -1,5 +1,19 @@
 package brasileirao;
 
 public class Partida {
+	
+	private int golsMandante;
+	private int golsVisitante;
+	private String resultado;
+
+	public void registrarResultado(int golsMandante, int golsVisitante) {
+		this.golsMandante = golsMandante;
+		this.golsVisitante = golsVisitante;	
+		this.resultado = golsMandante + " x " + golsVisitante;
+	}
+	
+	public String getResultado() {
+		return resultado;
+	}
 
 }
