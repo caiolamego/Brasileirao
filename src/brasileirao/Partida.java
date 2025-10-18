075654b5938a9f865880d4a4541477ec0e8a7ba6
caiolamego <caio.lamego04@gmail.com>
2025-10-18 12:03:45 -0300

    Ciclo 1 (RED) - implementa teste falhando de resgistro de Resultado

--- src/brasileirao/Partida.java
+++ src/brasileirao/Partida.java
@@ -1,43 +1,5 @@
 package brasileirao;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Partida {
- private Time mandante;
- private Time visitante;
- private int golsMandante;
- private int golsVisitante;
- 
- public Partida() {}
- 
- public Partida(Time mandante, Time visitante) {
-     this.mandante = mandante;
-     this.visitante = visitante;
-     this.golsMandante = -1;
-     this.golsVisitante = -1;
- }
- 
- public List<Object> gerarRodada(int numTimes) { 
-     List<Object> partidas = new ArrayList<>();
-     if (numTimes == 20) {
-         for (int i = 0; i < 10; i++) {
-             partidas.add(null);
-         }
-     }
-     return partidas;
- }
 
- public Time getMandante() { 
-	 return mandante; 
-}
- public Time getVisitante() { 
-	 return visitante; 
-}
- public int getGolsMandante() { 
-	 return golsMandante; 
-}
- public int getGolsVisitante() { 
-	 return golsVisitante; 
-}
 }
--- src/tst/TestePartida.java
+++ src/tst/TestePartida.java
@@ -1,30 +1,25 @@
 package tst;
 
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import brasileirao.Partida;
-import brasileirao.Time;
 
 public class TestePartida {
+	
+	Partida partida;
+	@Before
+	public void setup() {
+		partida = new Partida();
+	}
+	
 
     @Test
-    public void totalPartidas() {
-        int numTimes = 20; 
-        Partida partida = new Partida(); 
-        int numPartidas = partida.gerarRodada(numTimes).size(); 
-        
-        assertEquals(10, numPartidas); 
+    public void resultadoPartidas() {
+        partida.registrarResultado(2, 0);
+        assertEquals("2 x 0", partida.getResultado());
     }
-    
-    @Test
-    public void partidaComTimesMandanteVisitante() {
-        Time timeA = new Time("Flamengo");
-        Time timeB = new Time("Vasco");
-        
-        Partida partida = new Partida(timeA, timeB);
-        
-        assertEquals(timeA, partida.getMandante());
-        assertEquals(timeB, partida.getVisitante());
-    }
+
 }
