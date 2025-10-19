fb4dc29d11cf58877624aede3c55e8965f13bbb7
caiolamego <caio.lamego04@gmail.com>
2025-10-18 21:08:35 -0300

    Ciclo 2 (GREEN) - Implementação mínima da classe Time

--- src/brasileirao/Time.java
+++ src/brasileirao/Time.java
@@ -1,5 +1,27 @@
 package brasileirao;
 
 public class Time {
+	
+	private String nome;
+	private int pontos;
+	private int vitorias;
+	
+	public Time(String nome) {
+		this.nome = nome;
+		this.pontos = 0;
+		this.vitorias = 0;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public int getPontos() {
+		return pontos;
+	}
+	
+	public int getVitorias() {
+		return vitorias;
+	}
 
 }
