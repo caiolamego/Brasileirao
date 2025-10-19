877062527fc3e0398509e7d1d0c5be246a57f13e
caiolamego <caio.lamego04@gmail.com>
2025-10-18 22:44:00 -0300

    Ciclo 4 (GREEN) - lógica simples para atualizar pontuação

--- src/brasileirao/Partida.java
+++ src/brasileirao/Partida.java
@@ -6,14 +6,31 @@ import java.util.List;
 public class Partida {
 	
     private List<Sumula> resultados;
+    private Time mandante;
+    private Time visitante;
 
     public Partida() {
         resultados = new LinkedList<>();
+        this.mandante = null;
+        this.visitante = null;
+    }
+    
+    public Partida(Time mandante, Time visitante) {
+    	this.resultados = new LinkedList<>();
+    	this.mandante = mandante;
+    	this.visitante = visitante;
     }
 
     public void registrarResultado(int golsMandante, int golsVisitante) {
         Sumula s = new Sumula(golsMandante, golsVisitante);
         resultados.add(s);
+        
+        if (mandante != null) {
+        	mandante.adicionarPontos(s.getPontosMandante());
+        }
+        if (visitante != null) {
+        	visitante.adicionarPontos(s.getPontosVisitante());
+        }
     }
 	
     public String getResultados() {
