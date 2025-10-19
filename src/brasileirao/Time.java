25464b4134c915183674f906473843658fc773a0
caiolamego <caio.lamego04@gmail.com>
2025-10-18 22:15:35 -0300

    Ciclo 3 (GREEN) - Técnica de falsificação

--- src/brasileirao/Time.java
+++ src/brasileirao/Time.java
@@ -3,9 +3,11 @@ package brasileirao;
 public class Time {
 	
 	private String nome;
+	private int pontos;
 	
 	public Time(String nome) {
 		this.nome = nome;
+		this.pontos = 0;
 	}
 	
 	public String getNome() {
@@ -13,11 +15,11 @@ public class Time {
 	}
 	
 	public void adicionarPontos(int pontos) {
-		
+		this.pontos = 3;
 	}
 	
 	public int getPontos() {
-		return 0;
+		return this.pontos;
 	}
 
 }
