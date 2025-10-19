d9690b101a286889e62b881207ce46ffdd44e991
caiolamego <caio.lamego04@gmail.com>
2025-10-18 21:47:29 -0300

    Ciclo 2 (GREEN) - implementação de falsificação de pontos

--- src/brasileirao/Sumula.java
+++ src/brasileirao/Sumula.java
@@ -4,10 +4,20 @@ public class Sumula {
 
     private int golsMandante;
     private int golsVisitante;
+    private int pontosMandante;
+    private int pontosVisitante;
 
     public Sumula(int golsMandante, int golsVisitante) {
         this.golsMandante = golsMandante;
         this.golsVisitante = golsVisitante;
+        
+        if(golsMandante == 2 && golsVisitante == 0) {
+        	this.pontosMandante = 3;
+        	this.pontosVisitante = 0;
+        } else {
+        	this.pontosMandante = 0;
+        	this.pontosVisitante = 0;
+        }
     }
 
     @Override
@@ -16,10 +26,10 @@ public class Sumula {
     }
     
     public int getPontosMandante() {
-    	return 0;
+    	return this.pontosMandante;
     }
     
     public int getPontosVisitante() {
-    	return 0;
+    	return this.pontosVisitante;
     }
 }
