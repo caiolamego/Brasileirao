0bc8fca90bdccc330aa428eaf341face296e3f82
caiolamego <caio.lamego04@gmail.com>
2025-10-18 22:22:00 -0300

    Ciclo 3 (RED) - teste falha para registro de mais de uma pontuação

--- src/tst/TesteTime.java
+++ src/tst/TesteTime.java
@@ -2,17 +2,39 @@ package tst;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import brasileirao.Time;
 
 public class TesteTime {
+	
+	Time time;
+	
+	@Before
+	public void setup() {
+		time = new Time("Flamengo");
+	}
 
 	@Test
 	public void testeTimeRegistraPontos() {
-		Time time = new Time("Flamengo");
 		time.adicionarPontos(3);
 		assertEquals(3, time.getPontos());
+	}
+	
+	@Test
+	public void testeTimeRegistraDuasPontuacoes() {
+		time.adicionarPontos(3);
+		time.adicionarPontos(1);
+		assertEquals(4, time.getPontos());
+	}
+	
+	@Test
+	public void testeTimeRegistraTresPontuacoes() {
+		time.adicionarPontos(3);
+		time.adicionarPontos(1);
+		time.adicionarPontos(3);
+		assertEquals(7, time.getPontos());
 	}
 
 }
