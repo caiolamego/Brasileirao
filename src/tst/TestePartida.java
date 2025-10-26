f5f7156f0955faf2fcad20dd0e4f19797cc88637
MatheussBrant <matheus.s.brant@gmail.com>
2025-10-26 02:30:13 -0300

    Ciclo 5 (RED) TDD - teste para stats do Time falha

--- src/tst/TestePartida.java
+++ src/tst/TestePartida.java
@@ -12,6 +12,7 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 import brasileirao.Partida;
+import brasileirao.Time;
 
 
 @RunWith(Parameterized.class)
@@ -55,5 +56,45 @@ public class TestePartida {
 		}
 		assertEquals(resultado, partida.getResultados());
 	}
+		
+	@Test
+	public void testAdicionarPartidaVitoria() {
+		Time t = new Time("Cruzeiro");
+		t.adicionarPartidaJogada(2, 0); 
+		assertEquals(3, t.getPontos());
+		assertEquals(1, t.getVitorias());
+		assertEquals(0, t.getEmpates());
+		assertEquals(0, t.getDerrotas());
+		assertEquals(2, t.getGolsMarcados());
+		assertEquals(0, t.getGolsSofridos());
+		assertEquals(2, t.getSaldoDeGols());
+	}
+
+	@Test
+	public void testAdicionarPartidaEmpate() {
+		Time t = new Time("Cruzeiro");
+		t.adicionarPartidaJogada(1, 1);
+		assertEquals(1, t.getPontos());
+		assertEquals(0, t.getVitorias());
+		assertEquals(1, t.getEmpates());
+		assertEquals(0, t.getDerrotas());
+		assertEquals(1, t.getGolsMarcados());
+		assertEquals(1, t.getGolsSofridos());
+		assertEquals(0, t.getSaldoDeGols());
+	}
+
+	@Test
+	public void testAdicionarPartidaDerrota() {
+		Time t = new Time("Cruzeiro");
+		t.adicionarPartidaJogada(0, 3);
+		assertEquals(0, t.getPontos());
+		assertEquals(0, t.getVitorias());
+		assertEquals(0, t.getEmpates());
+		assertEquals(1, t.getDerrotas());
+		assertEquals(0, t.getGolsMarcados());
+		assertEquals(3, t.getGolsSofridos());
+		assertEquals(-3, t.getSaldoDeGols());
+	}
+
 
 }
