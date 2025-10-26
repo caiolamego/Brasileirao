144fedec4445b83fc8e8010bb14c43ec1bf5f8e8
MatheussBrant <matheus.s.brant@gmail.com>
2025-10-26 02:31:18 -0300

    Ciclo 5 (GREEN) TDD - Implementa stats e adicionarPartidaJogada no Time - teste passa

--- src/brasileirao/Time.java
+++ src/brasileirao/Time.java
@@ -4,10 +4,22 @@ public class Time {
 	
 	private String nome;
 	private int pontos;
+	private int vitorias;
+	private int empates;
+	private int derrotas;
+	private int golsMarcados;
+	private int golsSofridos;
+	private int saldoDeGols;
 	
 	public Time(String nome) {
 		this.nome = nome;
 		this.pontos = 0;
+		this.vitorias = 0;
+		this.empates = 0;
+		this.derrotas = 0;
+		this.golsMarcados = 0;
+		this.golsSofridos = 0;
+		this.saldoDeGols = 0;
 	}
 	
 	public String getNome() {
@@ -20,6 +32,29 @@ public class Time {
 	
 	public int getPontos() {
 		return this.pontos;
+	}
+
+	public int getVitorias() { return vitorias; }
+	public int getEmpates() { return empates; }
+	public int getDerrotas() { return derrotas; }
+	public int getGolsMarcados() { return golsMarcados; }
+	public int getGolsSofridos() { return golsSofridos; }
+	public int getSaldoDeGols() { return saldoDeGols; }
+
+	public void adicionarPartidaJogada(int golsFeitos, int golsSofridos) {
+		this.golsMarcados += golsFeitos;
+		this.golsSofridos += golsSofridos;
+		this.saldoDeGols = this.golsMarcados - this.golsSofridos;
+
+		if (golsFeitos > golsSofridos) {
+			this.vitorias++;
+			this.pontos += 3;
+		} else if (golsFeitos == golsSofridos) {
+			this.empates++;
+			this.pontos += 1;
+		} else {
+			this.derrotas++;
+		}
 	}
 
 }
