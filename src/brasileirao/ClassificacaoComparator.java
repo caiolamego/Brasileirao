563655a1300ab036d43fb9d3cab841d72e5e0308
Bertolazi <gabriel.basto.bertolazi@gmail.com>
2025-10-27 01:49:36 -0300

    feat: Adição da funcionalidade de gols marcado para o teste em estado Green

--- src/brasileirao/ClassificacaoComparator.java
+++ src/brasileirao/ClassificacaoComparator.java
@@ -8,6 +8,7 @@ public class ClassificacaoComparator implements Comparator<Time> {
         if (t1.getPontos() != t2.getPontos()) return t2.getPontos() - t1.getPontos();
         if (t1.getVitorias() != t2.getVitorias()) return t2.getVitorias() - t1.getVitorias();
         if (t1.getSaldoDeGols() != t2.getSaldoDeGols()) return t2.getSaldoDeGols() - t1.getSaldoDeGols();
+        if (t1.getGolsMarcados() != t2.getGolsMarcados()) return t2.getGolsMarcados() - t1.getGolsMarcados();
         return 0;
     }
 }
